0e2b9020fe333beef77d67064e0467be2c758edb
Dimuello <phoenix.design.room@gmail.com>
2024-05-04 21:09:53 +0300

    ver-1

--- app/src/main/java/com/example/rockpaperscissors/Main.kt
+++ app/src/main/java/com/example/rockpaperscissors/Main.kt
@@ -2,4 +2,17 @@ package com.example.rockpaperscissors
 
 fun main() {
 
+    var computerChoice = ""
+    println("Enter your choice: Rock, Paper or Scissors")
+    var userChoice = readln()
+
+    while (
+        userChoice != "Rock" &&
+        userChoice != "Paper" &&
+        userChoice != "Scissors"
+    ) {
+        println("Incorrect choice. Please enter Rock, Paper or Scissors")
+        userChoice = readln()
+    }
+
 }
